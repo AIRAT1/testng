2b06dfabb91ba9de2cbd1f51dd66e95efc53c9f8
Airat Khairullin <airat.khairullin@simbirsoft.com>
2021-09-14 16:34:28 +0300

    add appium

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,7 +4,6 @@ import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-import java.io.IOException;
 import java.net.URL;
 
 public abstract class BaseTest {
